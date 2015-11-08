96335605b8ad9f43817a168c696fe79f8010a089
budougumi0617 <budougumi0617@gmail.com>
2015-11-09 02:30:13 +0900

    refs #28 Add empty test code

---
+++ Chat/chatapplication/src/test/java/aikeda/chatapplication/ChatActivityTest.java
@@ -0,0 +1,43 @@
+package aikeda.chatapplication;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by budougumi0617 on 2015/11/08.
+ */
+public class ChatActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void testOnCreateOptionsMenu() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+
+    @Test
+    public void testOnClick() throws Exception {
+
+    }
+}
